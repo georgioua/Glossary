6c44509fd5ba44695adbb998b5b5fb99ced7c971
Andrew Georgiou <ageorgiou_77@hotmail.com>
2023-06-01 16:41:29 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using Application.AppGlossary;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Application.Core;
+using API.Extensisions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationService(builder.Configuration);
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("CorsPolicy");
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+
+}catch(Exception ex){
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during database migration");
+}
+app.Run();
+
